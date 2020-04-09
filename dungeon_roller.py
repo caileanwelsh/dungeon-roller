064d03ddbe39fe8402ac538c6ece0dfdcb6b6caf
Joshua <cussen.joshua@gmail.com>
2020-04-09 10:48:42 +0100

    improvements in efficiency and added option to list all dungeons

--- dungeon_roller.py
+++ dungeon_roller.py
@@ -10,81 +10,78 @@ default_easy_paths = 3
 default_medium_paths = 2
 default_hard_paths = 1
 
-dungeons = [ac, cm, ta, se, hotw, coe, cof, arah] # list of all dungeons
-forbidden_paths = [arah_s] # paths that are excluded from dungeon rolls
+easy_description = "Easy"
+medium_description = "Medium"
+hard_description = "Hard"
 
-
-# List out available dungeons of the desired difficulty and then pick one/some at random
-def gen_paths(desired_quantity, desired_difficulty):
-    rolled_dungeonpaths = []
-    available_dungeonpaths = []
-
-    # Generate list of available dungeonpaths
+def find_available_paths(allow_story, allow_arah, dungeons):
+    available_dungeonpaths = [[], [], []]
     for dungeon in dungeons:
         for path in dungeon.paths:
-            if path.difficulty == desired_difficulty and path not in forbidden_paths:
-                available_dungeonpaths.append(DungeonPath(dungeon, path))
+            if not allow_story and path.number == 0:
+                continue
+            if not allow_arah and path == arah_s:
+                continue
+            available_dungeonpaths[path.difficulty].append(DungeonPath(dungeon, path))
+    return available_dungeonpaths
 
-    # Roll on list of available dungeonpaths
+def roll_dungeonpaths(desired_quantity, available_dungeonpaths):
+    rolled_dungeonpaths = []
     while len(rolled_dungeonpaths) < desired_quantity:
         i = randint(0, len(available_dungeonpaths)-1)
         rolled_dungeonpaths.append(available_dungeonpaths.pop(i))
 
     # Return list of dungeonpaths sorted alphabetically by dungeon name and then in ascending order of path number
-    return sorted(rolled_dungeonpaths, key = lambda x: (x.dungeon.name, x.path.number))
+    return sorted(rolled_dungeonpaths, key = lambda dungeonpath: (dungeonpath.dungeon.name, dungeonpath.path.number))
 
-# Print dungeon paths so that they look pretty
-def print_dungeonpaths(dungeonpaths):
- for dungeonpath in dungeonpaths:
-    print("    {}".format(dungeonpath.str()))
+def print_dungeonpaths(dungeonpaths, description):
+    if len(dungeonpaths) > 0:
+        print("{} dungeon{}:".format(description, "" if len(dungeonpaths) == 1 else "s"))
 
-def print_discord_message(easy_paths, medium_paths, hard_paths):
+        for dungeonpath in dungeonpaths:
+            print((" " * 3),"{}".format(dungeonpath.str()))
+        print()
+
+def print_discord_message(easy_paths, medium_paths, hard_paths, date, time):
+    print("**— Guild Dungeons {} —**".format(date))
+    print("*Gather in the Guild Hall to form groups at {}*".format(time))
+    print()
+    print_dungeonpaths(easy_paths, easy_description)
+    print_dungeonpaths(medium_paths, medium_description)
+    print_dungeonpaths(hard_paths, hard_description)
+
+def list_all_dungeonpaths(dungeonpathslist):
+    print_dungeonpaths(dungeonpathslist[0], easy_description)
+    print_dungeonpaths(dungeonpathslist[1], medium_description)
+    print_dungeonpaths(dungeonpathslist[2], hard_description)
+
+def roll_dungeons(easy, medium, hard, available_dungeonpathslist):
+
+    # Set quantity of paths to max number of paths if quantity exceeds number of available paths
+    if easy > len(available_dungeonpathslist[0])-1:
+        easy = len(available_dungeonpathslist[0])-1
+    if medium > len(available_dungeonpathslist[1])-1:
+        medium = len(available_dungeonpathslist[1])-1
+    if hard > len(available_dungeonpathslist[2])-1:
+        hard = len(available_dungeonpathslist[2])-1
+
+    # ROLL DUNGEONS!!!
+    easy_paths = roll_dungeonpaths(easy, available_dungeonpathslist[0])
+    medium_paths = roll_dungeonpaths(medium, available_dungeonpathslist[1])
+    hard_paths = roll_dungeonpaths(hard, available_dungeonpathslist[2])
+
     today = datetime.date.today()
     # next friday is today if today is friday
     next_friday = today + datetime.timedelta((4-today.weekday()) % 7)
-    print("**— Guild Dungeons {} —**".format(next_friday))
-    print("*Gather in the Guild Hall to form groups at 20:00 CET/CEST*")
-    print()
-    if len(easy_paths) > 0:
-        print("Easy dungeon{}:".format("" if len(easy_paths) == 1 else "s"))
-        print_dungeonpaths(easy_paths)
-        print()
-    if len(medium_paths) > 0:
-        print("Medium dungeon{}:".format("" if len(medium_paths) == 1 else "s"))
-        print_dungeonpaths(medium_paths)
-        print()
-    if len(hard_paths) > 0:
-        print("Hard dungeon{}:".format("" if len(hard_paths) == 1 else "s"))
-        print_dungeonpaths(hard_paths)
 
-# Remove story paths from possible rolls or allow arah story
-def forbid_paths(forbid_story, allow_arah):
-    if forbid_story:
-        for dungeon in dungeons:
-            for path in dungeon.paths:
-                if path.number == 0 and path not in forbidden_paths:
-                    forbidden_paths.append(path)
-    if allow_arah:
-        forbidden_paths.remove(arah_s)
-
-# Count allowed paths by difficulty
-# Return dictionary of {difficulty : quantity}
-def count_paths_by_difficulty(dungeons):
-    # 1: easy, 2: medium, 3: hard
-    difficulty_totals = {1 : 0, 2: 0, 3:  0}
-    for dungeon in dungeons:
-        for path in dungeon.paths:
-            if path not in forbidden_paths:
-                difficulty_totals[path.difficulty]+=1
-    return difficulty_totals
+    print_discord_message(easy_paths, medium_paths, hard_paths, next_friday, "20:00 CET/CEST")
 
 def main():
     seed()
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-s", "--story",
-        action="store_false",
-        default=True,
+        action="store_true",
         help="include story paths in possible rolls (except arah)")
     parser.add_argument(
         "-a", "--arah",
@@ -105,25 +102,20 @@ def main():
         type=int,
         default=default_hard_paths,
         help="number of hard paths to roll (default {})".format(default_hard_paths))
+    parser.add_argument(
+        "-l", "--list",
+        action="store_true",
+        help="list all available dungeon paths")
     args = parser.parse_args()
 
-    forbid_paths(args.story, args.arah)
-    path_difficulty_totals = count_paths_by_difficulty(dungeons)
+    dungeons = [ac, cm, ta, se, hotw, coe, cof, arah] # list of all dungeons
 
-    # Set quantity of paths to max number of paths if quantity exceeds number of available paths
-    if args.easy > path_difficulty_totals[1]:
-        args.easy = path_difficulty_totals[1]
-    if args.medium > path_difficulty_totals[2]:
-        args.medium = path_difficulty_totals[2]
-    if args.hard > path_difficulty_totals[3]:
-        args.hard = path_difficulty_totals[3]
-
-    # ROLL DUNGEONS!!!
-    easy_paths = gen_paths(args.easy, 1)
-    medium_paths = gen_paths(args.medium, 2)
-    hard_paths = gen_paths(args.hard, 3)
-
-    print_discord_message(easy_paths, medium_paths, hard_paths)
+    if args.list:
+        available_dungeonpaths = find_available_paths(True, True, dungeons)
+        list_all_dungeonpaths(available_dungeonpaths)
+    else:
+        available_dungeonpaths = find_available_paths(args.story, args.arah, dungeons)
+        roll_dungeons(args.easy, args.medium, args.hard, available_dungeonpaths)
 
 if __name__ == "__main__":
     main()
--- dungeons.py
+++ dungeons.py
@@ -7,7 +7,7 @@ class Path:
     def __init__(self, name, number, difficulty, level):
         self.name = name
         self.number = number
-        self.difficulty = difficulty # 1-3, 1 is easy, 3 is hard
+        self.difficulty = difficulty # 0-2, 0 is easy, 2 is hard
         self.level = level # minimum character level expected for dungeon path, was used for sorting but not any more
 
 class DungeonPath:
@@ -19,53 +19,53 @@ class DungeonPath:
     def str(self):
         return "{:20s} - Path {:d} ({:s})".format(self.dungeon.name, self.path.number, self.path.name)
 
-ac_s = Path("Story",       0, 1, 30)
-ac_1 = Path("Hodgins",     1, 1, 35)
-ac_2 = Path("Detha",       2, 2, 35)
-ac_3 = Path("Tzark",       3, 1, 35)
+ac_s = Path("Story",       0, 0, 20)
+ac_1 = Path("Hodgins",     1, 0, 25)
+ac_2 = Path("Detha",       2, 1, 25)
+ac_3 = Path("Tzark",       3, 0, 25)
 ac = Dungeon("Ascalonian Catacombs", [ac_s, ac_1, ac_2, ac_3])
 
-cm_s = Path("Story",       0, 1, 40)
-cm_1 = Path("Asura",       1, 1, 45)
-cm_2 = Path("Seraph",      2, 2, 45)
-cm_3 = Path("Butler",      3, 2, 45)
+cm_s = Path("Story",       0, 0, 40)
+cm_1 = Path("Asura",       1, 0, 45)
+cm_2 = Path("Seraph",      2, 1, 45)
+cm_3 = Path("Butler",      3, 1, 45)
 cm = Dungeon("Caudecus Manor", [cm_s, cm_1, cm_2, cm_3])
 
-ta_s = Path("Story",       0, 1, 50)
-ta_1 = Path("Leurent",     1, 1, 55)
-ta_2 = Path("Vevina",      2, 1, 55)
-ta_3 = Path("Aetherpath",  3, 3, 55)
+ta_s = Path("Story",       0, 0, 50)
+ta_1 = Path("Leurent",     1, 0, 55)
+ta_2 = Path("Vevina",      2, 0, 55)
+ta_3 = Path("Aetherpath",  3, 2, 55)
 ta = Dungeon("Twilight Arbor", [ta_s, ta_1, ta_2, ta_3])
 
-se_s = Path("Story",       0, 1, 60)
-se_1 = Path("Fergg",       1, 1, 65)
-se_2 = Path("Rasolov",     2, 2, 65)
-se_3 = Path("Koptev",      3, 1, 65)
+se_s = Path("Story",       0, 0, 60)
+se_1 = Path("Fergg",       1, 0, 65)
+se_2 = Path("Rasolov",     2, 1, 65)
+se_3 = Path("Koptev",      3, 0, 65)
 se = Dungeon("Sorrow's Embrace", [se_s, se_1, se_2, se_3])
 
-cof_s = Path("Story",      0, 1, 70)
-cof_1 = Path("Ferrah",     1, 1, 75)
-cof_2 = Path("Magg",       2, 1, 75)
-cof_3 = Path("Rhiannon",   3, 2, 75)
+cof_s = Path("Story",      0, 0, 70)
+cof_1 = Path("Ferrah",     1, 0, 75)
+cof_2 = Path("Magg",       2, 0, 75)
+cof_3 = Path("Rhiannon",   3, 1, 75)
 cof = Dungeon("Citadel of Flame", [cof_s, cof_1, cof_2, cof_3])
 
-hotw_s = Path("Story",     0, 2, 76)
-hotw_1 = Path("Butcher",   1, 1, 80)
-hotw_2 = Path("Plunderer", 2, 2, 80)
-hotw_3 = Path("Zealot",    3, 2, 80)
+hotw_s = Path("Story",     0, 1, 76)
+hotw_1 = Path("Butcher",   1, 0, 80)
+hotw_2 = Path("Plunderer", 2, 1, 80)
+hotw_3 = Path("Zealot",    3, 1, 80)
 hotw = Dungeon("Honor of the Waves", [hotw_s, hotw_1, hotw_2, hotw_3])
 
-coe_s = Path("Story",      0, 1, 78)
-coe_1 = Path("Submarine",  1, 1, 80)
-coe_2 = Path("Teleporter", 2, 2, 80)
-coe_3 = Path("Front Door", 3, 1, 80)
+coe_s = Path("Story",      0, 0, 78)
+coe_1 = Path("Submarine",  1, 0, 80)
+coe_2 = Path("Teleporter", 2, 1, 80)
+coe_3 = Path("Front Door", 3, 0, 80)
 coe = Dungeon("Crucibal of Eternity", [coe_s, coe_1, coe_2, coe_3])
 
-arah_s = Path("Story",     0, 3, 80)
-arah_1 = Path("Jotun",     1, 3, 80)
-arah_2 = Path("Mursaat",   2, 3, 80)
-arah_3 = Path("Forgotten", 3, 3, 80)
-arah_4 = Path("Seer",      4, 3, 80)
+arah_s = Path("Story",     0, 2, 80)
+arah_1 = Path("Jotun",     1, 2, 80)
+arah_2 = Path("Mursaat",   2, 2, 80)
+arah_3 = Path("Forgotten", 3, 2, 80)
+arah_4 = Path("Seer",      4, 2, 80)
 arah = Dungeon("Ruined City of Arah", [arah_s, arah_1, arah_2, arah_3, arah_4])
 
 
